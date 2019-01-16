0097838f9d64ca2f8e1e48036f3ef4a545370e84
Nikolai Krivenko <nkrivenko35@gmail.com>
2019-01-17 02:02:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# VR Park ICO
+
+## Technologies used
+1. Ethereum blockchain as a base of infrastructure
+2. solc 0.5.0
+3. Truffle 5.0.1 for testing
+4. OpenZeppelin v2.1.1 contracts as a base of source code
+
---
+++ truffle-config.js
@@ -0,0 +1,31 @@
+require('babel-register');
+require('babel-polyfill');
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"// , // Match any network id
+    },
+    coverage: {
+      host: "127.0.0.1",
+      port: 8555,
+      network_id: "*", // Match any network id
+      gasPrice: 0x01,
+    },
+    rinkeby: {
+      host: "127.0.0.1",
+      port: 8547,
+      from: "0x6E09f0F026483f54928995D6fbb2AC4E6F24E12C",
+      network_id: 4,
+      gas: 4612388
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
+};
