0a43a969155e42cf8c1e5b7663f709b0631a5cd4
Nikolai Krivenko <nkrivenko35@gmail.com>
2019-01-17 02:16:23 +0300

    [#1] implement token contract

---
+++ contracts/VrParkToken.sol
@@ -0,0 +1,23 @@
+pragma solidity 0.5.0;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Capped.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+
+
+contract VrParkToken is ERC20Mintable, ERC20Capped, ERC20Detailed, Ownable {
+
+    uint8 internal constant DECIMALS = 18;
+
+    constructor (uint256 _cap) public ERC20Detailed("VR", "VR", DECIMALS)
+        ERC20Capped(_cap) {
+        // empty block
+    }
+
+    function removeMinter(address account) public onlyOwner {
+        _removeMinter(account);
+    }
+
+}
+
---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,10 @@
+var Migrations = artifacts.require("./Migrations.sol");
+var VrParkToken = artifacts.require("./VrParkToken.sol");
+
+module.exports = function(deployer, network, accounts) {
+    deployer.deploy(Migrations);
+
+    deployer.deploy(VrParkToken, 2000, { from: accounts[0] }).then(() => {
+        console.log('Deployed with address', VrParkToken.address);
+    });
+};
