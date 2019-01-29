9314a91bef4359745f20c1c4cc4a9e284c928da0
Nikolai Krivenko <nkrivenko35@gmail.com>
2019-01-30 01:13:32 +0300

    [INNER] use openzeppelin-test-helpers package

--- test/helpers/ether.js
+++
@@ -1,8 +0,0 @@
-export default function ether (n) {
-  return new web3.BigNumber(web3.toWei(n, 'ether'));
-}
-
-export function ether (n) {
-  return new web3.BigNumber(web3.toWei(n, 'finney'));
-}
-
